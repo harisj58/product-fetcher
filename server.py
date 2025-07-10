4f6de2b02fb3863a8a15c7c5e20e71747839c612
Haris Javed <harisj58@gmail.com>
2025-07-10 13:56:26 +0000

    feat: implement `/search` API endpoint

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from schema.models import SearchRequest
+from utils.search import search_google
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -10,3 +15,15 @@ def health_check():
         "message": "Server is running!",
     }
     return resp
+
+
+@app.post("/search")
+async def search(request: SearchRequest):
+    try:
+        res = await search_google(request.query, request.country)
+        return res
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": f"Some error occurred for your request - {str(e)}",
+        }
